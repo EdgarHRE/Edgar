0ae0499ab33f839ae8fada2b4245d86f08a7be08
EdgarHRE <82249746+EdgarHRE@users.noreply.github.com>
2021-04-12 19:25:13 -0500

    Add files via upload

---
+++ Programa/Grafo_Bibliopolium/arist.py
@@ -0,0 +1,7 @@
+class Arist:
+    def __init__(self,new_arist):
+        self.arist = new_arist
+    
+    def __repr__(self):
+        return f'<Arist:{self.arist}>'
+
---
+++ Programa/Grafo_Bibliopolium/nodes.py
@@ -0,0 +1,9 @@
+ 
+class Node:
+    def __init__(self,new_node):
+        self.node = new_node
+    
+    
+    def __repr__(self):
+        return f'{self.node}'
+        
