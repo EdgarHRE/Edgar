b1c72f50ec8628b19f79a72cd597aba86f8a43cc
EdgarHRE <82249746+EdgarHRE@users.noreply.github.com>
2021-06-07 22:24:38 -0500

    Delete Programa directory

--- Programa/Grafo_Bibliopolium/arist.py
+++
@@ -1,7 +0,0 @@
-class Arist:
-    def __init__(self,new_arist):
-        self.arist = new_arist
-    
-    def __repr__(self):
-        return f'<Arist:{self.arist}>'
-
--- Programa/Grafo_Bibliopolium/nodes.py
+++
@@ -1,9 +0,0 @@
- 
-class Node:
-    def __init__(self,new_node):
-        self.node = new_node
-    
-    
-    def __repr__(self):
-        return f'{self.node}'
-        
